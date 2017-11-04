467dabb88d305f4230fff8cfc2d30c0f8265ac76
saulvenskutonis <wensunato@gmail.com>
2017-11-04 21:36:52 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './js/test';
+
+import './scss/index.scss';
+
+ReactDOM.render(<Test />, document.getElementById('app'));
