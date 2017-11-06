085b4c2950a1e9e36f9c80f8e87d0abffd5d5da1
saulvenskutonis <wensunato@gmail.com>
2017-11-06 16:38:51 +0000

    Add editor config and stylelint

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,52 +2,54 @@ import path from 'path';
 
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import StyleLintPlugin from 'stylelint-webpack-plugin';
 
 const paths = {
-	DIST: path.resolve(__dirname, 'dist'),
-	SRC: path.resolve(__dirname, 'src'),
-	JS: path.resolve(__dirname, 'src/js')
+    DIST: path.resolve(__dirname, 'dist'),
+    SRC: path.resolve(__dirname, 'src'),
+    JS: path.resolve(__dirname, 'src/js'),
 };
 
 module.exports = {
-	entry: path.join(paths.SRC, 'index.js'),
-	output: {
-		path: paths.DIST,
-		filename: 'app.bundle.js'
-	},
+    entry: path.join(paths.SRC, 'index.js'),
+    output: {
+        path: paths.DIST,
+        filename: 'app.bundle.js',
+    },
 
-	module: {
-		rules: [{
-			test: /\.(js|jsx)$/,
-			exclude: /node_modules/,
-			use: [
-				'babel-loader'
-			]
-		},
-		{
-			test: /\.scss$/,
-			use: ExtractTextPlugin.extract({
-				fallback: 'style-loader',
-				use: ['css-loader', 'sass-loader']
-			})
-		}]
-	},
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: [
+                'babel-loader',
+            ],
+        }, {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader'],
+            }),
+        }],
+    },
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.join(paths.SRC, 'index.html'),
-			hash: true
-		}),
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(paths.SRC, 'index.html'),
+            hash: true,
+        }),
 
-		new ExtractTextPlugin('styles.bundle.css')
-	],
+        new StyleLintPlugin(),
 
-	devServer: {
-		contentBase: paths.SRC,
-		compress: true
-	},
+        new ExtractTextPlugin('styles.bundle.css'),
+    ],
 
-	resolve: {
-		extensions: ['.js', 'jsx']
-	}
+    devServer: {
+        contentBase: paths.SRC,
+        compress: true,
+    },
+
+    resolve: {
+        extensions: ['.js', 'jsx'],
+    },
 };
