6956a91e0f8aa78d1d79d995c93ca94bc8d92fdc
saulvenskutonis <saulvenskutonis@gmail.com>
2018-02-26 20:45:17 +0000

    Add typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,33 @@
-const {
-  name
-} = require('./package.json');
+const { name } = require('./package.json');
 
 module.exports = {
   pathPrefix: process.env.CI ? `/${name}` : `/`,
   siteMetadata: {
-    author: 'You!',
-    title: `Gatsby Default (Blog) Starter`
+    author: 'Hippocrypto',
+    title: `Hippocrypto`
   },
   plugins: [
     'gatsby-plugin-react-next',
     'gatsby-plugin-catch-links',
-    'gatsby-transformer-sharp', {
+    'gatsby-transformer-sharp',
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/pages`,
         name: 'pages'
       }
-    }, {
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-114325312-1',
         head: true
+      }
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: 'src/utils/typography/'
       }
     },
     'gatsby-transformer-remark',
